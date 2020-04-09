c757f82f0b489b60c095dd6c3795f8df920ee17b
Tony Antelman <AntelmanAnthonyJ@JohnDeere.com>
2020-04-09 10:24:30 -0500

    update using image file compression

--- mycars/models.py
+++ mycars/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+import sys
+from PIL import Image
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 class Year(models.Model):
     model_year = models.TextField()
@@ -14,6 +18,20 @@ class ModelType(models.Model):
     description = models.TextField(blank=True)
     image_file = models.ImageField(upload_to='images/', null=True, blank=True)
     image_url = models.TextField(max_length=200, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+        self.image_file = self.compressImage(self.image_file)
+    super(ModelType, self).save(*args, **kwargs)
+
+    def compressImage(self, image_file):
+        imageTemproary = Image.open(image_file)
+    outputIoStream = BytesIO()
+    imageTemproaryResized = imageTemproary.resize( (300,300) ) 
+    imageTemproary.save(outputIoStream , format='JPEG', quality=20)
+    outputIoStream.seek(0)
+    image_file = InMemoryUploadedFile(outputIoStream,'ImageField', "%s.jpg" % modeltype.name.split('.')[0], 'image/jpeg', sys.getsizeof(outputIoStream), None)
+    return image_file
 
     def __str__(self):
         return self.name
